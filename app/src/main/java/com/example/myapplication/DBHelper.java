debef28311fda6000a150626cba4b8f7b9341372
luenm03 <lumedina074@gmail.com>
2024-06-29 22:55:42 -0500

    xd

--- app/src/main/java/com/example/myapplication/DBHelper.java
+++ app/src/main/java/com/example/myapplication/DBHelper.java
@@ -30,7 +30,7 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        // Crear tabla de usuarios
+        // XDDDCrear tabla de usuarios
         db.execSQL("CREATE TABLE " + USER_TABLE_NAME + " (" +
                 USER_COLUMN_USERNAME + " TEXT PRIMARY KEY, " +
                 USER_COLUMN_PASSWORD + " TEXT)");
